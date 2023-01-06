88f6e4bf9c5c3313dda94e3d0c13bfe0a614374b
Sravan T P <sravantp03@gmail.com>
2023-01-06 15:28:11 +0530

    created file and folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
 import './App.css';
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
