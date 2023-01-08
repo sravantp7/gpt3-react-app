802ac128115286038ae8f5ee2ffb7d43c8268e4e
Sravan T P <sravantp03@gmail.com>
2023-01-08 17:42:31 +0530

    implemented Features component in the Container section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './features.css';
+import { featuresData } from './featuresData';
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='gpt3__features section__padding' id='features'>
+      <div className="gpt3__features-heading">
+        <h1 className='gradient__text'>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        {
+          featuresData.map((data, index) => (<Feature title={data.title} text={data.text} key={index}/> ))
+        }
+      </div>
+    </div>
   );
 }
 
